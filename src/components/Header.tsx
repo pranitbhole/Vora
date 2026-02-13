1372f7798bfaaf069e2e58563b7f7d22cfe7550f
Pranit Bhole <pranitbhole@gmail.com>
2026-02-13 17:29:36 +0530

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    const{theme,setTheme} =useTheme()
+    const isDark = theme ==="dark";
+
+    return(
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="container mx-auto flex h-16 items-center justify-between px-4">
+                <Link to="/">
+                    <img src={isDark ? "/logo-dark.png" : "/logo-light.png"} alt="Vora Logo" className="h-14" />
+                </Link>
+
+                <div>
+                   {/*Search*/}
+                   {/* theme toggle*/} 
+                   <div>
+                    <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                        className={`flex item-center cursor-pointer transition-transform duration-500
+                            ${isDark? "rotate-180": "rotate-0"}
+                            `}
+                        >
+                        
+                        {isDark? <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all"/>:
+                        <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all"/>}
+                    </div>
+                   </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
